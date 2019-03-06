54b84d2e98b8eff4104be424aca98a95a8e3783e
ILUHHA <iikleschin@gmail.com>
2019-03-06 15:26:15 +0300

    Первая попытка защиты

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var popup = document.querySelector(".modal-form");
 
 var close = popup.querySelector(".form-close");
 
-var name = popup.querySelector("[name=personal-name]");
+var yourname = popup.querySelector("[name=yourname]");
 
 var form = popup.querySelector("form");
 
@@ -14,7 +14,7 @@ var isStorageSupport = true;
 var storage = "";
 
 try {
-  storage = localStorage.getItem("name");
+  storage = localStorage.getItem("yourname");
 } catch (err) {
   isStorageSupport = false;
 }
@@ -24,10 +24,10 @@ link.addEventListener("click", function (evt) {
     popup.classList.add("modal-show");
     
     if (storage) {
-      name.value = storage;
+      yourname.value = storage;
       email.focus();
     } else {
-      name.focus();
+      yourname.focus();
     }
   });
 
@@ -39,14 +39,14 @@ close.addEventListener("click", function (evt) {
 
 
 form.addEventListener("submit", function (evt) {
-  if (!name.value || !email.value) {
+  if (!yourname.value || !email.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("name", name.value);
+      localStorage.setItem("yourname", yourname.value);
     }
   }
 });
@@ -86,3 +86,4 @@ window.addEventListener("keydown", function (evt) {
       }
     }
   });
+
